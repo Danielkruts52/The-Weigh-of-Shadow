00aaf05ed7da3de16133b64344a20ce67a4af077
root <root@weightofshadow.de>
2026-02-28 19:59:18 +0100

    Изменения

--- contact/models.py
+++ contact/models.py
@@ -7,16 +7,17 @@ class Support(models.Model):
         ('CA', 'CA')
     ]
     name = models.CharField(max_length=200)
-    type = models.CharField(choices=country,default='CA')
+    type = models.CharField(max_length=3, choices=country, default='CA')  # Добавил max_length=3
     phone_number = models.CharField(max_length=20)
     def __str__(self):
         return self.name
+
 class shop(models.Model):
     country = [
         ("CA","CA"),
         ("USA","USA")
     ]
-    type = models.CharField(choices=country,default='CA')
+    type = models.CharField(max_length=3, choices=country, default='CA')  # Добавил max_length=3
     city = models.CharField(max_length=100, default='')
     address = models.CharField(max_length=200)
     phone_number = models.CharField(max_length=20)
@@ -24,26 +25,26 @@ class shop(models.Model):
         return self.address
 
 class office(models.Model):
- country = [
+    country = [
         ("CA","CA"),
         ("USA","USA")
     ]
- name = models.CharField(max_length=100, default='')
- type = models.CharField(choices=country,default='CA')
- city = models.CharField(max_length=100, default='')
- address = models.CharField(max_length=200)
- phone_number = models.CharField(max_length=20)
- def __str__(self):
+    name = models.CharField(max_length=100, default='')
+    type = models.CharField(max_length=3, choices=country, default='CA')  # Добавил max_length=3
+    city = models.CharField(max_length=100, default='')
+    address = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=20)
+    def __str__(self):
         return self.name
- 
+
 class community(models.Model):
- country = [
+    country = [
         ("CA","CA"),
         ("USA","USA")
     ]
- type = models.CharField(choices=country,default='CA')
- city = models.CharField(max_length=100, default='')
- address = models.CharField(max_length=200)
- phone_number = models.CharField(max_length=20)
- def __str__(self):
+    type = models.CharField(max_length=3, choices=country, default='CA')  # Добавил max_length=3
+    city = models.CharField(max_length=100, default='')
+    address = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=20)
+    def __str__(self):
         return self.address
