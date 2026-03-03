1a21157672b53897c44b1833dff2cd717c284bd9
Danielkruts52 <danilfilimonenko57@gmail.com>
2026-03-03 14:11:00 +0300

    Создал модели футболок и толстовок

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,3 +2,6 @@ from django.contrib import admin
 from .models import *
 admin.site.register(Collections)
 admin.site.register(SpecialCollections)
+admin.site.register(T_shirt)
+admin.site.register(Sweatshirt)
+
--- catalog/models.py
+++ catalog/models.py
@@ -12,3 +12,43 @@ class SpecialCollections(models.Model):
     def __str__(self):
         return self.name
     
+class T_shirt(models.Model):
+    type = [
+        ("S", "S"),
+        ("M", "M"),
+        ("L", "L"),
+        ("XL", "XL")
+    ]
+    image = models.ImageField
+    image2 = models.URLField(max_length=250)
+    name = models.CharField(max_length=100)
+    structure = models.CharField(max_length=100, default='cotton')
+    price = models.CharField(max_length=20)
+    size = models.CharField(default='M', choices= type)
+    text = models.TextField('about')
+    def __str__(self):
+        return self.name
+
+class Sweatshirt(models.Model):
+    type = [
+        ("S", "S"),
+        ("M", "M"),
+        ("L", "L"),
+        ("XL", "XL")
+    ]
+    image = models.ImageField
+    image2 = models.URLField(max_length=250)
+    name = models.CharField(max_length=100)
+    structure = models.CharField(max_length=100, default='cotton')
+    price = models.CharField(max_length=20)
+    size = models.CharField(default='M', choices= type)
+    text = models.TextField('about')
+    def __str__(self):
+        return self.name
+
+
+
+
+
+    
+    
