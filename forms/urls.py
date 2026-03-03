a97254b8fc178e286cfccf0d82339c4b1d978f26
Danielkruts52 <danilfilimonenko57@gmail.com>
2026-03-03 10:44:47 +0300

    Создал все формы для бизнеса

--- forms/urls.py
+++ forms/urls.py
@@ -1,11 +1,15 @@
-
 from django.urls import path
-from .import views
+from . import views
+
 app_name = 'form'
+
 urlpatterns = [
-    path('vacancy', views.create_standart_vacancy,name='vacancy'),
-    path('young_vacancy',views.create_young_vacancy,name = 'young'),
-    path('vacancydisabled',views.create_vacancy,name='disabled'),
-    path('space', views.create_space, name = 'space'),
-    path('suc',views.Suc,name = 'suc')
+    path('vacancy', views.create_standart_vacancy, name='vacancy'),
+    path('young_vacancy', views.create_young_vacancy, name='young'),
+    path('vacancydisabled', views.create_vacancy, name='disabled'),
+    path('cooperation', views.create_coop, name='coop'),
+    path('investor', views.create_investor, name='invest'),
+    path('space', views.create_space, name='space'),
+    path('suc', views.Suc, name='suc'),
+    path('suc/<str:form_type>/', views.Suc, name='suc_with_type'),
 ]
--- forms/views.py
+++ forms/views.py
@@ -1,9 +1,37 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import DisabilityVacancyForm, VacancyForm, YoungVacancyForm, SpaceForm
+from .forms import DisabilityVacancyForm, VacancyForm, YoungVacancyForm, SpaceForm, InvestorForm, cooperationForm
 
-def Suc(request):
-    return render(request, 'forms_/success.html')
+def Suc(request, form_type=None):
+    # Словарь для сопоставления типов форм с их названиями
+    form_names = {
+        'standart': 'Standard vacancy',
+        'young': 'Youth Program',
+        'disability': 'For people with disabilities',
+        'space': 'space',
+        'investor': 'Investor',
+        'cooperation': 'Cooperation',
+    }
+    
+    # Словарь для сопоставления типов форм с URL для возврата
+    return_urls = {
+        'standart': 'form:vacancy',
+        'young': 'form:young',
+        'disability': 'form:disabled',
+        'space': 'form:space',
+        'investor': 'form:invest',
+        'cooperation': 'form:coop',
+    }
+    
+    # Получаем название формы или значение по умолчанию
+    form_display_name = form_names.get(form_type, 'Форма')
+    return_url = return_urls.get(form_type, 'form:disabled')
+    
+    return render(request, 'forms_/success.html', {
+        'form_type': form_type,
+        'form_display_name': form_display_name,
+        'return_url': return_url
+    })
 
 def create_standart_vacancy(request):
     if request.method == 'POST':
@@ -11,7 +39,7 @@ def create_standart_vacancy(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Your RESUME has been successfully submitted!')
-            return redirect('form:suc')
+            return redirect('form:suc_with_type', form_type='standart')
         else:
             # Для отладки
             print("Form errors:", form.errors)
@@ -27,13 +55,13 @@ def create_young_vacancy(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Your RESUME has been successfully submitted!')
-            return redirect('form:suc')
+            return redirect('form:suc_with_type', form_type='young')
         else:
             # Для отладки
             print("Form errors:", form.errors)
             messages.error(request, 'Please correct the errors below.')
     else:
-        form = YoungVacancyForm
+        form = YoungVacancyForm()
     
     return render(request, 'forms_/young_vacancy.html', {'form': form})
 
@@ -43,7 +71,7 @@ def create_vacancy(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Your RESUME has been successfully submitted!')
-            return redirect('form:suc')
+            return redirect('form:suc_with_type', form_type='disability')
         else:
             # Для отладки
             print("Form errors:", form.errors)
@@ -52,13 +80,14 @@ def create_vacancy(request):
         form = DisabilityVacancyForm()
     
     return render(request, 'forms_/vacancy_form.html', {'form': form})
+
 def create_space(request):
     if request.method == 'POST':
         form = SpaceForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             messages.success(request, 'Your RESUME has been successfully submitted!')
-            return redirect('form:suc')
+            return redirect('form:suc_with_type', form_type='space')
         else:
             # Для отладки
             print("Form errors:", form.errors)
@@ -67,3 +96,35 @@ def create_space(request):
         form = SpaceForm()
     
     return render(request, 'forms_/space.html', {'form': form})
+
+def create_investor(request):
+    if request.method == 'POST':
+        form = InvestorForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your RESUME has been successfully submitted!')
+            return redirect('form:suc_with_type', form_type='investor')
+        else:
+            # Для отладки
+            print("Form errors:", form.errors)
+            messages.error(request, 'Please correct the errors below.')
+    else:
+        form = InvestorForm()
+    
+    return render(request, 'forms_/investor.html', {'form': form})
+
+def create_coop(request):
+    if request.method == 'POST':
+        form = cooperationForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your RESUME has been successfully submitted!')
+            return redirect('form:suc_with_type', form_type='cooperation')
+        else:
+            # Для отладки
+            print("Form errors:", form.errors)
+            messages.error(request, 'Please correct the errors below.')
+    else:
+        form = cooperationForm()
+    
+    return render(request, 'forms_/com.html', {'form': form})
